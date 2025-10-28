492be601d1bd8f69f2c892a2a56f3a372c6b2aae
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2025-10-28 14:15:16 +0100

    Text editor prototype

--- arrows.py
+++ arrows.py
@@ -15,31 +15,6 @@ class HandlerArrows:
         self.idmap = {} if idmap is None else idmap
         self.data_program = data_program
 
-    def listen(self, event):
-        match self.data_program.action:
-            case state.Action.CREATE:
-                if event.type in {pg.MOUSEBUTTONDOWN, pg.MOUSEBUTTONUP} and event.button == 1:
-                    x, y = event.pos
-                    x = self.wrld.camera.x_abs(x)
-                    y = self.wrld.camera.y_abs(y)
-                    ids = self.buttons_rect.probe(x, y)
-                    if ids == [] or (self.stage > 0 and self.id_base in ids):
-                        self.stage = 0
-                        print("Not a valid node...")
-                        return
-                    elif self.stage == 0:
-                        self.id_base = ids[-1]
-                        index = self.aux.cards_idmap[self.id_base]
-                        self.xi = (self.wrld.cards_xf[index] + self.wrld.cards_xi[index]) / 2
-                        self.yi = (self.wrld.cards_yf[index] + self.wrld.cards_yi[index]) / 2
-                        self.stage = 1
-                    elif self.stage == 2:
-                        id_final = ids[-1]
-                        self.wrld.link(self.id_base, id_final, self.aux)
-                        self.stage = 0
-                elif event.type == pg.MOUSEMOTION and self.stage == 1:
-                    self.stage = 2
-
     def probe(self, x, y):
         WIDTH = 10
         ids_intersect_x = [
--- editor.py
+++ editor.py
@@ -1,7 +1,26 @@
+from dataclasses import dataclass
+from typing import Callable
+import pygame as pg
+
+
+@dataclass(slots=True, frozen=True)
+class CoBuffer:
+    buffer: list
+    encoder: Callable
+
+
 class Editor:
-    def __init__(self):
+    def __init__(self, bindings, *cobuffers):
+        self.bindings = bindings
+        self.cobuffers = cobuffers
         self.buffer = ""
         self.cur = 0
+
+    def listen(self, mod, key, unicode):
+        try:
+            self.bindings[mod][key](self)
+        except KeyError:
+            self.insert(unicode)
 
     def get_buffer(self):
         return buffer
@@ -22,7 +41,22 @@ class Editor:
             self.cur += 1
 
     def insert(self, new):
-        return self.buffer[:self.cur] + new + self.buffer[self.cur:]
+        self.buffer = self.buffer[:self.cur] + new + self.buffer[self.cur:]
+        for cobuf in self.cobuffers:
+            cobuf.buffer = cobuf.buffer[:self.cur] + list(map(cobuf.encoder, new)) + cobuf.buffer[self.cur:]
 
     def backspace(self):
-        return self.buffer[:self.cur-1] + self.buffer[self.cur:]
+        self.buffer = self.buffer[:self.cur-1] + self.buffer[self.cur:]
+        for cobuf in self.cobuffers:
+            cobuf.buffer = cobuf.buffer[:cobuf.cur-1] + cobuf.buffer[cobuf.cur:]
+
+
+BINDINGS_EMACS = {
+    pg.KMOD_NONE: {
+        pg.K_BACKSPACE: Editor.backspace
+    },
+    pg.KMOD_LCTRL: {
+        pg.K_f: Editor.move_right,
+        pg.K_b: Editor.move_right
+    }
+}
